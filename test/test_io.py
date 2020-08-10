8bd245721d9c79da9318e37d72658fe6de1cb2d7
Austin Cory Bart <acbart@vt.edu>
2020-08-10 10:15:15 -0400

    Test for mocking input.

--- test/test_io.py
+++ test/test_io.py
@@ -1,1 +1,20 @@
 import io
+from unittest.mock import patch
+
+def haha(*args, **kwargs):
+    print("TEST")
+    return 'haha'
+
+overridden_modules = {
+    '__builtins__': {
+        'input': haha
+    }
+}
+
+s = patch.dict('sys.modules', overridden_modules)
+s.start()
+d = {'input': haha}
+c = compile('print(input())', "test.py", 'exec')
+exec(c, d)
+s.stop()
+print(d)
