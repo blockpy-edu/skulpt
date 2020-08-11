6bcd514b3b0f4c508db144302ffc05590bbc6f0d
Austin Cory Bart <acbart@vt.edu>
2020-08-11 12:44:57 -0400

    Some tests related to recent commits about infinite loops and mocking.

--- test/test_infinite_loop.py
+++ test/test_infinite_loop.py
@@ -1,6 +1,13 @@
 import sys
 sys.setExecutionLimit(1000)
 
+print("Gonna infinite loop myself, brb.")
+try:
+    while True:
+        pass
+except TimeoutError:
+    print("Timed it out")
+
 quit = False
 while not quit:
     quit = "quit" == input("Type 'quit' to quit.").lower()
